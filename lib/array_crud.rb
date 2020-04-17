676580a83f7dda034b7bace5609916711c68dce0
Maysoon Tabali <mtab03@me.com>
2020-04-17 22:41:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
    array = ["wow","I","am","really","learning"]
-   element = ("arrays!")
-end
+   element = .push("arrays!")
+end 
 
 def add_element_to_start_of_array(array, element)
   ["blue", "green", "pink", "purple"]
