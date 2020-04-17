14d9be5137099713edbd9a34eb2951df443dd358
Maysoon Tabali <mtab03@me.com>
2020-04-17 23:02:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,8 +9,6 @@ end
 def add_element_to_end_of_array(array, element)
   array << "arrays!"
   array.push ("arrays!")
-
-   element =("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
