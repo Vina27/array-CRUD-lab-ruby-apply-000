502193c6ca84fafbcba98638f8bfd0d81ccf55bd
Maysoon Tabali <mtab03@me.com>
2020-04-17 21:42:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  ["blue", "green", "pink", "purple"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["wow", "I", "am", "really", "learning"]
+  add_element_to_end_of_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["blue", "green", "pink", "purple"]
+  add_element_to_end_of_array.unshift("black")
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
